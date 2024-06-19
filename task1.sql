b615d389b93b3da196be749130a95567310f48de
alona <aloyna.vasilieva@gmail.com>
2024-06-19 14:20:23 +0200

    add request 1

---
+++ task1.sql
@@ -0,0 +1,8 @@
+//https://western-appeal-39b.notion.site/GenTech-Git-Summary-May-29-2024-97dcabaa9fb64f87b2853650e719058c
+
+SELECT DISTINCT
+Products.ProductName
+FROM Products
+left join OrderDetails 
+on Products.ProductID = OrderDetails.ProductID
+where OrderDetails.ProductID is NULL
