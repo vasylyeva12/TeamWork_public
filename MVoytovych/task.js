42b7cd3e5c04a9a267ac88288c6be32bda4806a2
VoytovychM <mary.voytovych@gmail.com>
2024-06-20 10:18:52 +0200

    commit

---
+++ MVoytovych/task.js
@@ -0,0 +1,13 @@
+--Вывести ко-во видео-роликов продолжительностью до 2 ч (не вкл.), используя метод aggregate()--
+
+db.videos.aggregate([
+
+    // input: all documents
+    {$match: {duration_secs: {$lt: 120 * 60 }
+    // output: filtered documents with duration less than to hours 
+    }
+    },
+    // input: filtered documents with duration less than to hours 
+    {$count:"total_number_of_videos_with_duration_2_hours"}
+    // output: the quantity of documents 
+])
