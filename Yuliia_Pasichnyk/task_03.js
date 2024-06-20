7c0c829105324d0ea83f7af5dbea019ceac1df50
Julia <pasichnyk.julia2@gmail.com>
2024-06-20 11:55:36 +0200

    update

--- Yuliia_Pasichnyk/task_03.js
+++ Yuliia_Pasichnyk/task_03.js
@@ -1,7 +1,7 @@
 db.users.aggregate([
     {
         $sort: {
-            баланс_юзера: -1
+            balance: -1
         }
     },
     {
@@ -10,8 +10,8 @@ db.users.aggregate([
     {
         $project: {
             _id: 0,
-            имя_юзера: 1,
-            баланс_юзера: 1
+            fullname: 1,
+            balance: 1
         }
     }
 ])
