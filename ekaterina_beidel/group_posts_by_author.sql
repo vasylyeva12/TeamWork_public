405e784c4f7eba61a9e73eca3475e25a9b529b88
ekaterinabeidel <ek.beidel@gmail.com>
2024-06-26 14:27:39 +0200

    group_posts_by_author.sql corrected

--- ekaterina_beidel/group_posts_by_author.sql
+++ ekaterina_beidel/group_posts_by_author.sql
@@ -1,6 +1,7 @@
-SELECT
-    author_id, COUNT(*) AS total_posts
-FROM
-    posts
-GROUP BY
-    author_id
+SELECT author_id, count(*) AS total_posts
+from posts
+         JOIN social_network.users on users.id = posts.author_id
+WHERE is_published is true
+GROUP BY users.id
+HAVING total_posts < 100
+ORDER BY total_posts DESC;
