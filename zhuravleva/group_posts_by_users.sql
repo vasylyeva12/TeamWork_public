d7997deb85b602345f575008a0dcca7b1ac7a1fb
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-06-26 14:21:43 +0200

    add extra requirements

--- zhuravleva/group_posts_by_users.sql
+++ zhuravleva/group_posts_by_users.sql
@@ -2,4 +2,6 @@ SELECT fullname, count(*) AS total_posts
 from posts
          JOIN social_network.users u on u.id = posts.author_id
 WHERE is_published is true
-GROUP BY u.id;
+GROUP BY u.id
+HAVING total_posts < 100
+ORDER BY total_posts DESC;
