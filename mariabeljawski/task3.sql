432df7caff8124c985f9561aced0b8b8dadab287
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-06-26 14:21:17 +0200

    Task is ready! inkl. change

--- mariabeljawski/task3.sql
+++ mariabeljawski/task3.sql
@@ -1,5 +1,6 @@
--- **Задача. Вывести общее ко-во активных постов для каждого автора**
--- проекция: `имя_автора`, `ко-во_постов`
+-- **Задача. Вывести общее ко-во активных постов для каждого автора, у которых их до 100 (не вкл.), в порядке убывания (по**
+
+-- -- проекция: имя_автора, ко-во_постов
 
 SELECT 
 users.fullname,
@@ -9,4 +10,8 @@ Left JOIN Users ON posts.author_id = users.id
 WHERE
 is_published = true
 Group BY 
-users.fullname
+author_id
+HAVING
+total_posts < 100
+order by
+total_posts DESC
