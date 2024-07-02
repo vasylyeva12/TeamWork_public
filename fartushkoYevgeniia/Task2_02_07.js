6e173d3674605100804b609d628c4f1cdb0abb44
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-02 13:34:51 +0200

    add task2

--- fartushkoYevgeniia/Task2_02_07.js
+++ fartushkoYevgeniia/Task2_02_07.js
@@ -1,6 +1,7 @@
 db.users.countDocuments(
     {
         country: { $nin: ['Germany', 'USA'] },
-        balance: { $gte: 10, $lte: 1000 }
+        balance: { $gte: 10, $lte: 1000 },
+        is_blocked: {$ne: true}
     }
 )
