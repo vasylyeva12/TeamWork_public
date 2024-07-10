2154b8ded9176e2028c974ffa5ea1837bc30cfd6
alona <aloyna.vasilieva@gmail.com>
2024-07-10 13:14:52 +0200

    added collection products

---
+++ AlonaVasylieva/task_1(10.07.24).js
@@ -0,0 +1,20 @@
+db.products.insertMany([
+    {
+          _id: 1,
+          title: 'Title1',
+          price: 280,
+          quantity: 4
+      },
+    {
+          _id: 2,
+          title: 'title2',
+          price: 300,
+          quantity: 8
+  },
+  {
+          _id: 3,
+          title: 'Title3',
+          price: 500,
+          quantity: 3
+  }
+])
