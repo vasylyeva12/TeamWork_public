43eefff0cac0fae9865471345baf09292edafc46
NataliaPnk <nataliaa.pnnk@gmail.com>
2024-07-10 13:37:38 +0200

    update

--- natalia_panasenko/task2.js
+++ natalia_panasenko/task2.js
@@ -1,4 +1,4 @@
 db.users.deleteMany(
-  { country: { $in: ['China', 'Brazil'] }, is_blocked: { $ne: true }, balance: { $gte: 0 } },
+  { country: { $in: ['China', 'Brazil'] }, is_blocked: { $ne: true }, balance: { $lt: 0 } },
   { _id: 0 }
 )
